c6a307f9278d4aa0a0a7e644a30970c36f46ae6d
Screight <seguer.matias@gmail.com>
2022-03-07 14:29:36 +0100

    Lunes Unity

---
+++ Assets/Scripts/Groundcheck.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Groundcheck : MonoBehaviour
+{
+    Player playerScript;
+    Rigidbody2D playerRigidBody;
+
+    void Start()
+    {
+        playerScript = GetComponentInParent<Player>();
+        playerRigidBody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Idle si estás en suelo || DENTRO de plataforma NO saltando/cayendo
+        if ((collision.gameObject.tag == "floor" || collision.gameObject.tag == "enemy"))
+        {
+            playerScript.IsGrounded = true;
+            playerRigidBody.gravityScale = playerScript.Gravity1 / Physics2D.gravity.y;
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, 0);
+
+            if (playerScript.State != PLAYER_STATE.RUN && playerScript.State != PLAYER_STATE.ATTACK)
+            {
+                if (playerScript.State == PLAYER_STATE.FALL)
+                {
+                    playerScript.State = PLAYER_STATE.IDLE;
+                    playerScript.SetPlayerAnimation(PLAYER_ANIMATION.LAND);
+                }
+
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((collision.gameObject.tag == "floor" || collision.gameObject.tag == "platform"))
+        {
+            playerScript.IsGrounded = false;
+
+            playerRigidBody.gravityScale = playerScript.Gravity2 / Physics2D.gravity.y;
+        } 
+    }
+}
+
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, ATTACK, DEATH }
-public enum PLAYER_ANIMATION { IDLE, RUN, JUMP, FALL, ATTACK, DEATH, STOP, BLOCK, HIT, LAST_NO_USE }
+public enum PLAYER_ANIMATION { IDLE, RUN, JUMP, FALL, LAND, ATTACK, DEATH, STOP, BLOCK, HIT, LAST_NO_USE }
 
 public class Player : MonoBehaviour
 {
@@ -24,12 +24,24 @@ public class Player : MonoBehaviour
 
     PLAYER_STATE m_state;
 
-    Rigidbody2D m_rb2d;
-    [SerializeField] float m_movementSpeed = 10.0f;
+    Rigidbody2D m_rb2D;
+    [SerializeField] float m_speed = 10.0f;
+    [SerializeField] float m_maxHeight = 10.0f;
+    [SerializeField] float m_timeToPeak1 = 1f;
+    [SerializeField] float m_timeToPeak2 = 1f;
+
     bool m_isGrounded;
 
     SpriteRenderer m_spriteRenderer;
     bool m_isFacingRight;
+
+    float m_gravity1;
+    float m_gravity2;
+    float m_initialVelocityY;
+    float m_direction;
+    bool m_canIMove = true;
+
+    
 
     private void Awake()
     {
@@ -43,12 +55,19 @@ public class Player : MonoBehaviour
         m_animationHash[(int)PLAYER_ANIMATION.BLOCK] = Animator.StringToHash(m_blockAnimationName);
         m_animationHash[(int)PLAYER_ANIMATION.HIT] = Animator.StringToHash(m_hitAnimationName);
 
-        m_rb2d = GetComponent<Rigidbody2D>();
+        m_rb2D = GetComponent<Rigidbody2D>();
         m_animator = GetComponent<Animator>();
         m_spriteRenderer = GetComponent<SpriteRenderer>();
 
         m_isGrounded = true;
         m_isFacingRight = true;
+    }
+
+    private void Start()
+    {
+        m_gravity1 = -2 * m_maxHeight / (m_timeToPeak1 * m_timeToPeak1);
+        m_gravity2 = -2 * m_maxHeight / (m_timeToPeak2 * m_timeToPeak2);
+        m_initialVelocityY = 2 * m_maxHeight / m_timeToPeak1;
     }
 
     void ChangeAnimationState(int p_newState)
@@ -61,40 +80,113 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        switch (m_state)
+         switch (m_state)
         {
             case PLAYER_STATE.IDLE:
                 Move(PLAYER_STATE.IDLE);
+                Jump();
                 break;
             case PLAYER_STATE.RUN:
                 Move(PLAYER_STATE.IDLE);
+                Jump();
+                break;
+            case PLAYER_STATE.JUMP:
+                Move(PLAYER_STATE.JUMP);
                 break;
         }
     }
 
     void Move(PLAYER_STATE p_defaultState)
     {
-        float direction = Input.GetAxisRaw("Horizontal");
-
-        if(direction != 0)
+        float horizontalAxisValue = Input.GetAxisRaw("Horizontal");
+        if (horizontalAxisValue != 0 && m_canIMove)
         {
-            transform.position += new Vector3(direction * m_movementSpeed * Time.deltaTime, 0, 0);
-            
-            if (m_isGrounded)
+            if (!m_isFacingRight && horizontalAxisValue > 0) { FlipX(); }
+            if (m_isFacingRight && horizontalAxisValue < 0) { FlipX(); }
+
+            if (m_isGrounded && /*m_state != PLAYER_STATE.LAND &&*/ m_state != PLAYER_STATE.ATTACK)
             {
                 m_state = PLAYER_STATE.RUN;
-                ChangeAnimationState(m_animationHash[(int)PLAYER_STATE.RUN]);
+                SetPlayerAnimation(PLAYER_ANIMATION.RUN);
+                //ChangeAnimationState(m_animationHash[(int)PLAYER_ANIMATION.RUN]);
             }
-
-            if(direction < 0 && m_isFacingRight){ m_spriteRenderer.flipX = true; m_isFacingRight = !m_isFacingRight; }
-            if(direction > 0 && !m_isFacingRight){ m_spriteRenderer.flipX = false; m_isFacingRight = !m_isFacingRight; }
         }
         else
         {
             m_state = p_defaultState;
-            ChangeAnimationState(m_animationHash[(int)PLAYER_STATE.IDLE]);
+            if (m_state != PLAYER_STATE.ATTACK && m_animationCurrentState != (int)PLAYER_ANIMATION.LAND)
+            {
+                SetPlayerAnimation((PLAYER_ANIMATION)p_defaultState);
+                //ChangeAnimationState(m_animationHash[(int)p_defaultState]);
+            }
         }
 
+        if (m_canIMove)
+        {
+            m_direction = horizontalAxisValue;
+            m_rb2D.velocity = new Vector2(m_direction * m_speed, m_rb2D.velocity.y);
+        }
+
+        if (m_rb2D.velocity.y < 0 && m_state != PLAYER_STATE.ATTACK)
+        {
+            m_rb2D.gravityScale = m_gravity2 / Physics2D.gravity.y;
+            m_state = PLAYER_STATE.FALL;
+            SetPlayerAnimation(PLAYER_ANIMATION.FALL);
+        }
+
+        if (m_rb2D.velocity.y < -200) { m_rb2D.velocity = new Vector2(m_rb2D.velocity.x, -200.0f); }
     }
+
+    void Jump()
+    {
+        if (InputManager.Instance.JumpButtonPressed && m_isGrounded)
+        {
+            m_rb2D.gravityScale = m_gravity1 / Physics2D.gravity.y;
+            m_rb2D.velocity = new Vector2(m_rb2D.velocity.x, m_initialVelocityY);
+
+            m_state = PLAYER_STATE.JUMP;
+            SetPlayerAnimation(PLAYER_ANIMATION.JUMP);
+            m_isGrounded = false;
+        }
+    }
+
+    void FlipX()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        m_isFacingRight = !m_isFacingRight;
+    }
+
+    public void SetPlayerAnimation(PLAYER_ANIMATION p_animation)
+    {
+        ChangeAnimationState(m_animationHash[(int)p_animation]);
+    }
+
+    public PLAYER_ANIMATION GetPlayerAnimation() { return (PLAYER_ANIMATION)m_animationCurrentState; }
+    public PLAYER_STATE State
+    {
+        get { return m_state; }
+        set
+        {
+            m_state = value;
+            if (m_state == PLAYER_STATE.ATTACK) { m_rb2D.velocity = new Vector2(m_speed, m_rb2D.velocity.y); }
+        }
+    }
+
+    public float Gravity1
+    {
+        get { return m_gravity1; }
+    }
+
+    public float Gravity2
+    {
+        get { return m_gravity2; }
+    }
+    public bool IsGrounded
+    {
+        set { m_isGrounded = value; }
+        get { return m_isGrounded; }
+    }
+
+    public Vector3 Speed { get { return m_rb2D.velocity; } }
 
 }
